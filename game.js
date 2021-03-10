616b4466d0cf2123d1ce44c7d118982220055f8f
taruvar-mittal <mittaltaruvar@gmail.com>
2021-03-10 14:28:14 +0530

    mario entry

--- game.js
+++ game.js
@@ -5,7 +5,20 @@ const render = {
         // draw sky
         gameObj.tool.fillStyle = "#00b1eb";
         gameObj.tool.fillRect(0, 0, window.innerWidth, window.innerHeight);
-        gameObj.tool.drawImage(castleImage, 40, 40, 200, 150);
+        // gameObj.tool.drawImage(castleImage, 40, 40, 200, 150);
+
+        let mario = gameObj.entities.mario;
+        gameObj.tool.drawImage(
+            mario.sprite.img,
+            mario.sprite.srcX,
+            mario.sprite.srcY,
+            mario.sprite.srcW,
+            mario.sprite.srcH,
+            mario.posX,
+            mario.posY,
+            mario.width,
+            mario.height
+         )
     }
 }
 
@@ -16,11 +29,18 @@ class Game{
       canvas.height = window.innerHeight;
       canvas.width = window.innerWidth;
       const tool = canvas.getContext("2d");
+      let entities = {};
 
       // represent current state of game
       let gameObj = {
-          tool,canvas
+          tool,canvas,
+          entities
       }
+
+      tool.scale(2.5,2.5);
+
+     let mario = new Mario(spriteSheetImage, 175, 200, 18, 18);
+     gameObj.entities.mario = mario;
 
       render.init(gameObj);
     }
---
+++ mario.js
@@ -0,0 +1,27 @@
+class Sprite{
+    constructor(img, srcX, srcY, srcW, srcH){
+       this.img = img;
+       this.srcX = srcX;
+       this.srcY = srcY;
+       this.srcW = srcW;
+       this.srcH = srcH;
+    }
+}
+
+class Entity{
+    constructor(sprite, type, posX, posY, width, height){
+        this.sprite = sprite;
+        this.type = type;
+        this.posX = posX;
+        this.posY = posY;
+        this.width = width;
+        this.height = height;
+    }
+}
+
+class Mario extends Entity{
+    constructor(spritesheet, posX, posY, width, height){
+       let img = new Sprite(spritesheet, 650, 3, 17, 19);
+       super(img, "mario", posX, posY, width, height);
+    }
+}
